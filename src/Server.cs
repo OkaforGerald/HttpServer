61352ed00f08711c5747c397d5a97c9796ebc9b8
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-13 21:16:40 +0100

    Threaded

--- src/Server.cs
+++ src/Server.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
@@ -7,45 +8,63 @@ using static System.Net.WebRequestMethods;
 Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-var client = server.AcceptTcpClient();
-var response = "";
-var stream = client.GetStream();
-var reader = new StreamReader(stream);
-var writer = new StreamWriter(stream);
-
-var request = new List<string>();
-string line;
-while(!string.IsNullOrEmpty(line = reader.ReadLine()))
+var serverThread = new Thread(new ThreadStart(() =>
 {
-    request.Add(line);
+    server.Start();
+
+    while (true)
+    {
+        var client = server.AcceptTcpClient();
+
+        var clientThread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
+        clientThread.Start(client);
+    }
+}));
+
+serverThread.Start();
+
+void HandleClientCommunication(object tcpClient)
+{
+    var client = (TcpClient)tcpClient;
+    var response = "";
+    var stream = client.GetStream();
+    var reader = new StreamReader(stream);
+    var writer = new StreamWriter(stream);
+
+    var request = new List<string>();
+    string line;
+    while (!string.IsNullOrEmpty(line = reader.ReadLine()))
+    {
+        request.Add(line);
+    }
+
+    var tokens = request[0].Split(' ');
+    var path = tokens[1];
+
+    if (path == "/")
+    {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else if (path.Contains("/echo/"))
+    {
+        var str = path.Split('/')[^1];
+        response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {str.Length}\r\n\r\n{str}";
+    }
+    else if (path.Contains("/user-agent"))
+    {
+        var header = request.FirstOrDefault(x => x.StartsWith("User-Agent:"));
+        var value = header.Split(':')[1].Trim();
+        response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {value.Length}\r\n\r\n{value}";
+    }
+    else
+    {
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+
+    writer.AutoFlush = true;
+    writer.NewLine = "\r\n";
+
+    writer.WriteLine(response);
+
+    stream.Close();
 }
-
-var tokens = request[0].Split(' ');
-var path = tokens[1];
-
-if (path == "/")
-{
-    response = "HTTP/1.1 200 OK\r\n\r\n";
-}
-else if(path.Contains("/echo/"))
-{
-    var str = path.Split('/')[^1];
-    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {str.Length}\r\n\r\n{str}";
-}else if (path.Contains("/user-agent"))
-{
-    var header = request.FirstOrDefault(x => x.StartsWith("User-Agent:"));
-    var value = header.Split(':')[1].Trim();
-    response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {value.Length}\r\n\r\n{value}";
-}
-else
-{
-    response = "HTTP/1.1 404 Not Found\r\n\r\n";
-}
-
-writer.AutoFlush = true;
-writer.NewLine = "\r\n";
-
-writer.WriteLine(response);
-
-stream.Close();
