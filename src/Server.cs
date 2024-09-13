25fb6d534e28248c1d757dc03c791b99bc30f5e1
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-13 17:08:29 +0100

    Path

--- src/Server.cs
+++ src/Server.cs
@@ -6,18 +6,30 @@ Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-
 var client = server.AcceptTcpClient();
 var response = "";
 var stream = client.GetStream();
-byte[] buffer = new byte[1024];
+byte[] buffer = new byte[1];
+var memStream = new MemoryStream();
 
-while (stream != null)
+while (stream != null && await stream.ReadAsync(buffer, 0, buffer.Length) == 1)
 {
-    await stream.ReadAsync(buffer, 0, buffer.Length);
+    switch (buffer[0])
+    {
+        case (byte)'\n':
+            stream = null;
+            break;
+
+        case (byte)'\r':
+            break;
+
+        default:
+            memStream.WriteByte(buffer[0]);
+            break;
+    }
 }
 
-var request = Encoding.UTF8.GetString(buffer);
+var request = Encoding.UTF8.GetString(memStream.ToArray());
 
 var tokens = request.Split(' ');
 var path = tokens[1];
@@ -39,5 +51,3 @@ writer.NewLine = "\r\n";
 writer.WriteLine(response);
 
 stream.Close();
-
-
