faba1bf38d455495230e9e0f6bda90a477091f7b
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-13 15:53:54 +0100

    200 Response

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Logs from your program will appear here!");
 
@@ -10,6 +11,8 @@ var stream = client.GetStream();
 
 
 var writer = new StreamWriter(stream);
+writer.AutoFlush = true;
+writer.NewLine = "\r\n";
 
 writer.WriteLine("HTTP/1.1 200 OK\r\n\r\n");
 
