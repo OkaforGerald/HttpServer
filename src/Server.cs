9eea9fa2201a57968e99fdb81fad3479394b32b8
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-13 22:14:41 +0100

    file

--- src/Server.cs
+++ src/Server.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 Console.WriteLine("Logs from your program will appear here!");
 
@@ -45,12 +46,32 @@ void HandleClientCommunication(object tcpClient)
     {
         response = "HTTP/1.1 200 OK\r\n\r\n";
     }
-    else if (path.Contains("/echo/"))
+    else if (path.StartsWith("/echo"))
     {
         var str = path.Split('/')[^1];
         response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {str.Length}\r\n\r\n{str}";
+    }else if (path.StartsWith("/files"))
+    {
+        var file = path.Split('/')[1];
+        if (File.Exists($"{Directory.GetCurrentDirectory}/tmp/{file}"))
+        {
+            byte[] buffer = new byte[1024];
+            var info = new FileInfo($"{Directory.GetCurrentDirectory}/tmp/{file}");
+            using(var fstream = info.OpenRead())
+            {
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+
+            var content = Encoding.UTF8.GetString(buffer);
+
+            response = $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {info.Length}\r\n\r\n{content}";
+        }
+        else
+        {
+            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
     }
-    else if (path.Contains("/user-agent"))
+    else if (path.StartsWith("/user-agent"))
     {
         var header = request.FirstOrDefault(x => x.StartsWith("User-Agent:"));
         var value = header.Split(':')[1].Trim();
