e2cffc38c8053f4a2816c756631855d329677169
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-13 15:48:21 +0100

    Respond with 200

--- src/Server.cs
+++ src/Server.cs
@@ -5,4 +5,12 @@ Console.WriteLine("Logs from your program will appear here!");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket();
+var client = server.AcceptTcpClient();
+var stream = client.GetStream();
+
+
+var writer = new StreamWriter(stream);
+
+writer.WriteLine("HTTP/1.1 200 OK\r\n\r\n");
+
+
