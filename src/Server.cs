cbfcc35d33453d4b571a080edfb7fef6a27e93f3
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-14 14:39:49 +0100

    Post

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Sockets;
@@ -5,6 +6,11 @@ using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
 using File = System.IO.File;
+
+string NotFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+string PrepareOkResponse(string? ContentType = null, int? Length = null, string? Body = null) => Body is null ? "HTTP/1.1 200 OK\r\n\r\n" : $"HTTP/1.1 200 OK\r\nContent-Type: {ContentType}\r\nContent-Length: {Length}\r\n\r\n{Body}";
+string CreatedResponse = "HTTP/1.1 201 Created\r\n\r\n";
+
 
 Console.WriteLine("Logs from your program will appear here!");
 
@@ -41,46 +47,55 @@ void HandleClientCommunication(object tcpClient)
 
     var tokens = request[0].Split(' ');
     var path = tokens[1];
+    var requestBody = request[^1];
     var args = Environment.GetCommandLineArgs();
     if (path == "/")
     {
-        response = "HTTP/1.1 200 OK\r\n\r\n";
+        response = PrepareOkResponse();
     }
     else if (path.StartsWith("/echo"))
     {
         var str = path.Split('/')[^1];
-        response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {str.Length}\r\n\r\n{str}";
-    }else if (path.StartsWith("/files"))
+        response = PrepareOkResponse(ContentType: "text/plain", Length: str.Length, Body: str);
+    }else if (path.StartsWith("/files") && tokens[0].Equals("GET"))
     {
         var file = path.Split('/')[^1];
-        Console.WriteLine(Path.Combine(args[2], file));
+
         if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
         {
             byte[] buffer = new byte[1024];
-            var info = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(),args[2], file));
-            using(var fstream = info.OpenRead())
+            int Size = 0;
+            using(var fstream = File.OpenRead(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
             {
                 fstream.Read(buffer, 0, buffer.Length);
+                Size = (int)fstream.Length;
             }
 
             var content = Encoding.UTF8.GetString(buffer);
 
-            response = $"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {info.Length}\r\n\r\n{content}";
+            response = PrepareOkResponse(ContentType: "application/octet-stream", Length: Size, Body: content);
         }
         else
         {
-            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            response = NotFoundResponse;
         }
+    }else if(path.StartsWith("/files") && tokens[0].Equals("POST"))
+    {
+        var file = path.Split('/')[^1];
+
+        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), args[2], file), requestBody);
+
+        response = CreatedResponse;
     }
     else if (path.StartsWith("/user-agent"))
     {
         var header = request.FirstOrDefault(x => x.StartsWith("User-Agent:"));
         var value = header.Split(':')[1].Trim();
-        response = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {value.Length}\r\n\r\n{value}";
+        response = PrepareOkResponse(ContentType: "text/plain", Length: value.Length, Body: value);
     }
     else
     {
-        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        response = NotFoundResponse;
     }
 
     writer.AutoFlush = true;
