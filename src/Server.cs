95124cb181dece9f2c9ffcc9a427caccbbc871d9
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-14 14:43:41 +0100

    Post

--- src/Server.cs
+++ src/Server.cs
@@ -48,6 +48,7 @@ void HandleClientCommunication(object tcpClient)
     var tokens = request[0].Split(' ');
     var path = tokens[1];
     var requestBody = request[^1];
+    Console.WriteLine(String.Join("," ,request));
     var args = Environment.GetCommandLineArgs();
     if (path == "/")
     {
