edac1dbe40de34aa0937eed6ead6ab82aa0b7394
OkaforGerald <102563116+OkaforGerald@users.noreply.github.com>
2024-09-15 17:37:43 +0100

    test refactor

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using codecrafters_http_server.src;
 using System.Drawing;
 using System.Net;
 using System.Net.Http.Headers;
@@ -7,136 +8,160 @@ using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
 using File = System.IO.File;
 
-string NotFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-string PrepareOkResponse(string? ContentType = null, int? Length = null, string? Body = null) => Body is null ? "HTTP/1.1 200 OK\r\n\r\n" : $"HTTP/1.1 200 OK\r\nContent-Type: {ContentType}\r\nContent-Length: {Length}\r\n\r\n{Body}";
-string CreatedResponse = "HTTP/1.1 201 Created\r\n\r\n";
-
-
-Console.WriteLine("Logs from your program will appear here!");
-
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-var serverThread = new Thread(new ThreadStart(() =>
+public class Server
 {
-    server.Start();
+    int port;
+    public string NotFoundResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
+    public string PrepareOkResponse(string? ContentType = null, int? Length = null, string? Body = null) => Body is null ? "HTTP/1.1 200 OK\r\n\r\n" : $"HTTP/1.1 200 OK\r\nContent-Type: {ContentType}\r\nContent-Length: {Length}\r\n\r\n{Body}";
+    public string CreatedResponse = "HTTP/1.1 201 Created\r\n\r\n";
 
-    while (true)
+    public Server(int port)
     {
-        var client = server.AcceptTcpClient();
-
-        var clientThread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
-        clientThread.Start(client);
+        this.port = port;
     }
-}));
 
-serverThread.Start();
+    public Server() : this(4221) { }
 
-async void HandleClientCommunication(object tcpClient)
-{
-    var client = (TcpClient)tcpClient;
-    var response = "";
-    var stream = client.GetStream();
-
-    var writer = new StreamWriter(stream);
-
-    string line;
-
-    var buffer = new byte[1024];
-    stream.ReadTimeout = 1000;
-    List<byte> requestData = new List<byte>();
-
-    try
+    public void Run()
     {
-        int bytesRead;
-        do
+        Console.WriteLine("Logs from your program will appear here!");
+
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        var serverThread = new Thread(new ThreadStart(() =>
         {
-            bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-            if (bytesRead > 0)
+            server.Start();
+
+            while (true)
             {
-                requestData.AddRange(new ArraySegment<byte>(buffer, 0, bytesRead));
+                var client = server.AcceptTcpClient();
+
+                var clientThread = new Thread(new ParameterizedThreadStart(HandleClientCommunication));
+                clientThread.Start(client);
             }
-        } while (bytesRead > 0 && !requestData.Contains((byte)'\n'));
+        }));
+
+        serverThread.Start();
     }
-    catch (IOException)
+    
+
+    async void HandleClientCommunication(object tcpClient)
     {
-        Console.WriteLine("Stream Timeout!");
-        if (requestData.Count == 0)
+        var client = (TcpClient)tcpClient;
+        var response = "";
+        var stream = client.GetStream();
+
+        var writer = new StreamWriter(stream);
+
+        string line;
+
+        var buffer = new byte[1024];
+        stream.ReadTimeout = 1000;
+        List<byte> requestData = new List<byte>();
+
+        try
         {
-            return; // No data received, just return
+            int bytesRead;
+            do
+            {
+                bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead > 0)
+                {
+                    requestData.AddRange(new ArraySegment<byte>(buffer, 0, bytesRead));
+                }
+            } while (bytesRead > 0 && !requestData.Contains((byte)'\n'));
         }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error reading from stream: {ex.Message}");
-        return;
-    }
-
-    line = Encoding.UTF8.GetString(requestData.ToArray());
-
-
-    var request = line.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-    request[^1] = request[^1].Replace("\0", "");
-
-    var tokens = request[0].Split(' ');
-    var path = tokens[1];
-    var requestBody = request[^1];
-    //Log
-    Console.WriteLine(String.Join("," ,request));
-
-    var args = Environment.GetCommandLineArgs();
-    if (path == "/")
-    {
-        response = PrepareOkResponse();
-    }
-    else if (path.StartsWith("/echo"))
-    {
-        var str = path.Split('/')[^1];
-        response = PrepareOkResponse(ContentType: "text/plain", Length: str.Length, Body: str);
-    }else if (path.StartsWith("/files") && tokens[0].Equals("GET"))
-    {
-        var file = path.Split('/')[^1];
-
-        if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
+        catch (IOException)
         {
-            byte[] fbuffer = new byte[1024];
-            int Size = 0;
-            using(var fstream = File.OpenRead(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
+            Console.WriteLine("Stream Timeout!");
+            if (requestData.Count == 0)
             {
-                fstream.Read(fbuffer, 0, fbuffer.Length);
-                Size = (int)fstream.Length;
+                return;
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading from stream: {ex.Message}");
+            return;
+        }
 
-            var content = Encoding.UTF8.GetString(fbuffer);
+        line = Encoding.UTF8.GetString(requestData.ToArray());
 
-            response = PrepareOkResponse(ContentType: "application/octet-stream", Length: Size, Body: content);
+
+        var request = line.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+        var ctx = HttpContext.ParseRequest(line);
+        request[^1] = request[^1].Replace("\0", "");
+
+        var tokens = request[0].Split(' ');
+        var path = tokens[1];
+        var requestBody = request[^1];
+
+        //Log
+        Console.WriteLine(String.Join("\n", request));
+
+        var args = Environment.GetCommandLineArgs();
+        
+        if (path.StartsWith("/echo"))
+        {
+            var str = path.Split('/')[^1];
+            response = PrepareOkResponse(ContentType: "text/plain", Length: str.Length, Body: str);
+        }
+        else if (path.StartsWith("/files") && tokens[0].Equals("GET"))
+        {
+            var file = path.Split('/')[^1];
+
+            if (File.Exists(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
+            {
+                byte[] fbuffer = new byte[1024];
+                int Size = 0;
+                using (var fstream = File.OpenRead(Path.Combine(Directory.GetCurrentDirectory(), args[2], file)))
+                {
+                    fstream.Read(fbuffer, 0, fbuffer.Length);
+                    Size = (int)fstream.Length;
+                }
+
+                var content = Encoding.UTF8.GetString(fbuffer);
+
+                response = PrepareOkResponse(ContentType: "application/octet-stream", Length: Size, Body: content);
+            }
+            else
+            {
+                response = NotFoundResponse;
+            }
+        }
+        else if (path.StartsWith("/files") && tokens[0].Equals("POST"))
+        {
+            var file = path.Split('/')[^1];
+
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), args[2], file), requestBody);
+
+            response = CreatedResponse;
         }
         else
         {
             response = NotFoundResponse;
         }
-    }else if(path.StartsWith("/files") && tokens[0].Equals("POST"))
-    {
-        var file = path.Split('/')[^1];
 
-        File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), args[2], file), requestBody);
+        if (response.Equals(NotFoundResponse))
+        {
+            try
+            {
+                response = RouteHandler.RouteRequest(ctx);
+            }
+            catch (Exception ex)
+            {
+                response = NotFoundResponse;
+            }
+        }
 
-        response = CreatedResponse;
+        writer.AutoFlush = true;
+        writer.NewLine = "\r\n";
+
+        await writer.WriteLineAsync(response);
+
+        client.Close();
     }
-    else if (path.StartsWith("/user-agent"))
-    {
-        var header = request.FirstOrDefault(x => x.StartsWith("User-Agent:"));
-        var value = header.Split(':')[1].Trim();
-        response = PrepareOkResponse(ContentType: "text/plain", Length: value.Length, Body: value);
-    }
-    else
-    {
-        response = NotFoundResponse;
-    }
 
-    writer.AutoFlush = true;
-    writer.NewLine = "\r\n";
-
-    await writer.WriteLineAsync(response);
-
-    client.Close();
-    //stream.Close();
+    public void MapGet(string Path, Func<HttpContext, string> Action) => RouteHandler.RegisterRoute(Path, "GET", Action);
+    public void MapPost(string Path, Func<HttpContext, string> Action) => RouteHandler.RegisterRoute(Path, "POST", Action);
 }
